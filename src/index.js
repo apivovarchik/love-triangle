1bf61cd3ee8d5ea2a43686f5b4e84187669ff241
apivovarchik <sf_beton81@mail.ru>
2018-09-17 02:57:26 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var loveTrianglesCount=0;
+  for (var index = 0; index < preferences.length; index++) {
+    var firstLove = preferences[index];
+    if (firstLove == 0 || firstLove == index + 1) {
+      continue;
+    } else {
+      var secondLove = preferences[firstLove - 1];
+      if (secondLove == 0 || secondLove == firstLove) {
+        continue;
+      } else {
+        var thirdLove = preferences[secondLove - 1];
+        if (index + 1 == thirdLove) {
+        loveTrianglesCount++;
+        }
+      }
+    }
+  }
+  return loveTrianglesCount/3;
 };
